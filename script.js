53b5216c17049982523222f244357db5b7a50da0
MineNIR-Official <102046856+MineNIR-Official@users.noreply.github.com>
2023-03-07 20:29:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+// Получаем элементы DOM
+const colorDisplay = document.getElementById("color-display");
+const generateButton = document.getElementById("generate-color");
+const colorCode = document.getElementById("color-code");
 
+// Функция для генерации случайного цвета
+function generateRandomColor() {
+  const hexValues = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hexValues[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Обработчик события для кнопки генерации цвета
+generateButton.addEventListener("click", () => {
+  // Генерируем случайный цвет
+  const randomColor = generateRandomColor();
+  // Устанавливаем цвет дива отображения цвета
+  colorDisplay.style.backgroundColor = randomColor;
+  // Устанавливаем текст кода цвета
+  colorCode.textContent = `Код цвета: ${randomColor}`;
+});
